f85aee1e33456201ec5b8f94b9f811c7c8ad3711
BryanWho <brayanromeroariza@gmail.com>
2024-08-21 19:12:11 -0500

    listo

--- src/calculadora.py
+++ src/calculadora.py
@@ -15,7 +15,7 @@ def calcular(numero1, numero2, operacion):
             respuesta = numero1 / numero2
 
         # TODO: DEBES COLOCAR TU CÓDIGO AQUÍ# TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO, COMPROBANDO LOS SÍMBOLOS ^ Y %.
-        case '%':
+        case '^':
             respuesta = numero1 ** numero2
             
         case '%':
